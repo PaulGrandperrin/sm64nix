ae40dc5408a60d97de7908f7cdca7d064e1fe37b
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-04-14 13:48:05 +0200

    add legacy default target to test garnix

--- flake.nix
+++ flake.nix
@@ -147,6 +147,7 @@
         };
     in {
       packages.x86_64-linux.default = sm64pc {texture_pack = "reloaded"; options = ["HIGH_FPS_PC=1"];};
+      defaultPackage.x86_64-linux = sm64pc {};
       packages.x86_64-linux.sm64pc_us = sm64pc {rom_version = "us";};
       packages.x86_64-linux.sm64pc_eu = sm64pc {rom_version = "eu";};
       packages.x86_64-linux.sm64pc_jp = sm64pc {rom_version = "jp";};
